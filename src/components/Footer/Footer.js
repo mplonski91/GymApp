64ad5e1fc35a9bcf00eb018e6cc5046ace5c97f4
Maciek <maciej.plonski@pagepro.pl>
2019-02-01 14:33:56 +0100

    Create Layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Layout } from "antd";
+const { Footer } = Layout;
+
+const FooterPage = () => <Footer>Footer</Footer>;
+
+export default FooterPage;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Layout } from "antd";
+const { Header } = Layout;
+
+const HeaderPage = () => <Header style={{ color: "#fff" }}>AppJUK</Header>;
+
+export default HeaderPage;
